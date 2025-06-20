2f080d2bb0cb8096c571dec05f6e8fdda152fb8e
sejung2 <twindrop@naver.com>
2025-06-20 17:44:42 +0900

    feat: controller&routes

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are warching for a movie made after: ${searchingYear}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: string) {
+    return `This will return on movie with the id: ${movieId}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return movieData;
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  path(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
+  }
+}
